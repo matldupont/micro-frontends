c1820cb43e5b63ff2b5463ac7a1d3fd63987357d
Mat Dupont <dupont.mat.l@gmail.com>
2021-07-27 15:38:53 -0400

    forcing refresh

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import { MarketingApp } from "./components/MarketingApp";
 export function App() {
   return (
     <div>
-      <h1>Nav is here</h1>
+      <h1>Nav is here!</h1>
       <hr />
       <MarketingApp />
     </div>
